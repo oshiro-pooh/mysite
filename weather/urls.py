93df7ca26b40e115abffa3f4b42f8ebce51384a6
oshiro-pooh <oshirofindme@gmail.com>
2019-05-06 06:00:23 +0000

    天気機能作成(画面雛形のみ作成)

--- weather/urls.py
+++ weather/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'whether'
+app_name = 'weather'
 
 urlpatterns = [
+    path('index', views.index, name='index'),
+    path('next', views.next, name='next'),
     path('', views.index, name='index'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,4 +1,11 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def index(request):
-    return HttpResponse("Hello Django world!")
+    return render(request, 'weather/index.html')
+
+@login_required
+def next(request):
+    return render(request, 'weather/update.html')
