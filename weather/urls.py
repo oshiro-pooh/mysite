6f6f9c3966f5495aa54fe47e93329e6086c66add
oshiro-pooh <oshirofindme@gmail.com>
2019-05-06 11:15:41 +0000

    都道府県名/市町村データ登録処理実装中

--- weather/urls.py
+++ weather/urls.py
@@ -6,5 +6,6 @@ app_name = 'weather'
 urlpatterns = [
     path('index', views.index, name='index'),
     path('next', views.next, name='next'),
+    path('update', views.update, name='update'),
     path('', views.index, name='index'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,11 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Pref,City
+import urllib.request
+import urllib.parse
+import xml.etree.ElementTree as ET
 
 @login_required
 def index(request):
-    return render(request, 'weather/index.html')
+  return render(request, 'weather/index.html')
 
 @login_required
 def next(request):
-    return render(request, 'weather/update.html')
+  return render(request, 'weather/update.html')
+
+@login_required
+def update(request):
+  # テーブルを初期化
+  prefs = Pref.objects.all()
+  prefs.delete();
+  citys = City.objects.all()
+  citys.delete()
+  
+  # livedoor提供のcity１IDが定義されたxmlファイルを抽出
+  url = 'http://weather.livedoor.com/forecast/rss/primary_area.xml'
+  req = urllib.request.Request(url)
+  with urllib.request.urlopen(req) as response:
+    xml_string = response.read()
+  root = ET.fromstring(xml_string)
+
+  # タグを抽出し、テーブルに登録する
+  for pref in root.iter('pref'):
+    pref_entity = Pref(name=pref.attrib['title'])
+    pref_entity.save()
+    # for city in pref.iter('city'):
+    #  print(city.attrib)
+  return render(request, 'weather/update.html')
